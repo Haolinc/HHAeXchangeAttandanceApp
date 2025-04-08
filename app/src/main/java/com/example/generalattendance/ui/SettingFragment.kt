07ebf1c9f71bfc8d70555393f42ba37cdb76b42a
DESKTOP-S2SKHJS\forsk <forskyrimdemacia@gmail.com>
2025-04-07 20:35:41 -0400

    Restructure Setting page

--- app/src/main/java/com/example/generalattendance/ui/SettingFragment.kt
+++ app/src/main/java/com/example/generalattendance/ui/SettingFragment.kt
@@ -4,15 +4,18 @@ import android.provider.Settings
 import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -26,11 +29,9 @@ import com.example.generalattendance.enums.RouteEnum
 fun SettingFragment(navController: NavController, settingRouteList: List<NavigationData>){
     val context = LocalContext.current
     LazyColumn(
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
-            .padding(10.dp)
+            .padding(top = 20.dp)
     ){
         items(settingRouteList){ element ->
             when (element.route) {
@@ -59,9 +60,12 @@ fun SettingFragment(navController: NavController, settingRouteList: List<Navigat
 
 @Composable
 fun SettingBox(onNavigate: () -> Unit, stringResourceId: Int){
-    Box(
-        modifier = Modifier.clickable(onClick = onNavigate)
+    Column(
+        modifier = Modifier
+            .clickable(onClick = onNavigate)
+            .fillMaxWidth()
     ){
-        Text(text = stringResource(stringResourceId), fontSize = 20.sp)
+        Text(text = stringResource(stringResourceId), fontSize = 20.sp, modifier = Modifier.padding(horizontal = 40.dp, vertical = 20.dp))
+        HorizontalDivider(color = Color.Black, thickness = 1.dp, modifier = Modifier.padding(horizontal = 30.dp))
     }
 }
