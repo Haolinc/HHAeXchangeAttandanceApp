1ac4ae9fb3d03ef49def674a79fab6957112f699
DESKTOP-S2SKHJS\forsk <forskyrimdemacia@gmail.com>
2025-04-09 12:49:57 -0400

    built PermisisonGuideFragment

--- app/src/main/java/com/example/generalattendance/ui/PermissionGuideFragment.kt
+++ app/src/main/java/com/example/generalattendance/ui/PermissionGuideFragment.kt
@@ -2,9 +2,15 @@ package com.example.generalattendance.ui
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,7 +21,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.example.generalattendance.R
@@ -40,22 +50,33 @@ fun PermissionGuideFragment(onNavigate: () -> Unit){
         }
     }
     Column(
-        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
+//        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 30.dp, vertical = 100.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp)
     ){
-        Text("Need Permission")
-        RequestPermissionButton(context)
-    }
-}
+        Image(
+            painter =  painterResource(R.drawable.write_setting_guide_image),
+            contentDescription = "Write Setting Guide Image",
+            modifier = Modifier
+        )
+        Spacer(modifier = Modifier.height(20.dp))
 
-@Composable
-fun RequestPermissionButton(context: Context){
-    Button(
-        {
-            PermissionHelper.requestWriteSettingPermission(context)
+        Text(
+            text = stringResource(R.string.fragment_permission_rational_text),
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = {
+                PermissionHelper.requestWriteSettingPermission(context)
+            },
+            modifier = Modifier.fillMaxWidth(),
+        ){
+            Text(text = stringResource(R.string.fragment_permission_request_permission_button_text), fontSize = 20.sp)
         }
-    ){
-        Text("Request Permission")
     }
 }
