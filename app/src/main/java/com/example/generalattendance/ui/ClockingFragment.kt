fb230f7e7f9e4379cbead8a164dcbe0e3f45ca61
DESKTOP-S2SKHJS\forsk <forskyrimdemacia@gmail.com>
2025-04-10 11:13:45 -0400

    correct dial number header text position, now it will be relative to employee number head text

--- app/src/main/java/com/example/generalattendance/ui/ClockingFragment.kt
+++ app/src/main/java/com/example/generalattendance/ui/ClockingFragment.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -317,7 +318,7 @@ fun TopSectionTitleText(content: String, modifier: Modifier){
         fontSize = 25.sp,
         textAlign = TextAlign.Center,
         fontWeight = FontWeight.Bold,
-        modifier = modifier
+        modifier = modifier.wrapContentHeight()
     )
 }
 
